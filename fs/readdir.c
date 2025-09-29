e0097b934bf294582fd3a1df49b9f1fd2c30332c
Lup Gabriel <gwolf2u.website@gmail.com>
2025-09-29 18:48:15 +0300

    fs: readdir: Fix -Wunused-variable warning
    fs/readdir.c:294:16: error: unused variable 'inode' [-Werror,-Wunused-variable]
             struct inode *inode;
                           ^~~~~

--- fs/readdir.c
+++ fs/readdir.c
@@ -291,7 +291,7 @@ static int filldir(struct dir_context *ctx, const char *name, int namlen,
 	int prev_reclen;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	struct inode *inode;
+	struct inode __maybe_unused *inode;
 #endif
 	buf->error = verify_dirent_name(name, namlen);
 	if (unlikely(buf->error))
